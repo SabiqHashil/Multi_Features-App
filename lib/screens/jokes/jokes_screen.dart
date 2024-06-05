6de3f17ecbdf0408517c25921d89b046e6fa82c3
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2024-06-05 18:42:47 +0530

    updated

--- lib/screens/jokes/jokes_screen.dart
+++ lib/screens/jokes/jokes_screen.dart
@@ -14,6 +14,7 @@ class _JokesScreenState extends State<JokesScreen> {
   late Future<String> _jokeFuture;
   final JokesApiClient _jokesApiClient = JokesApiClient();
   final DBHelper _dbHelper = DBHelper();
+  bool _isLoading = false;
 
   @override
   void initState() {
@@ -24,7 +25,12 @@ class _JokesScreenState extends State<JokesScreen> {
   // Function to fetch random joke
   Future<void> _fetchRandomJoke() async {
     setState(() {
+      _isLoading = true;
       _jokeFuture = _jokesApiClient.fetchRandomJoke();
+    });
+    await _jokeFuture; // Wait for the joke to be fetched
+    setState(() {
+      _isLoading = false;
     });
   }
 
@@ -32,14 +38,21 @@ class _JokesScreenState extends State<JokesScreen> {
   Future<void> _saveJoke(String joke) async {
     await _dbHelper.insert('jokes', {'joke': joke});
     ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('Joke saved successfully')),
+      SnackBar(
+        content: const Text('Joke saved successfully!'),
+        behavior: SnackBarBehavior.floating,
+        margin: const EdgeInsets.only(top: 50.0, left: 10.0, right: 10.0),
+        duration: const Duration(seconds: 2),
+      ),
     );
   }
 
   // Function to retrieve saved jokes
   Future<List<String>> _getSavedJokes() async {
     final List<Map<String, dynamic>> jokes = await _dbHelper.queryAll('jokes');
-    return jokes.map((joke) => joke['joke'] as String).toList();
+    return jokes.reversed
+        .map((joke) => joke['joke'] as String)
+        .toList(); // Reverse to show the latest first
   }
 
   @override
@@ -50,65 +63,92 @@ class _JokesScreenState extends State<JokesScreen> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            FutureBuilder<String>(
-              future: _jokeFuture,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const CircularProgressIndicator();
-                } else if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                } else if (snapshot.hasData) {
-                  return JokeWidget(snapshot.data!);
-                } else {
-                  return const Text('No joke found');
-                }
-              },
-            ),
-            const Spacer(), // To push the button to the bottom
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.lightBlueAccent,
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: FutureBuilder<String>(
+                future: _jokeFuture,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const CircularProgressIndicator();
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else if (snapshot.hasData) {
+                    return JokeWidget(snapshot.data!);
+                  } else {
+                    return const Text('No joke found');
+                  }
+                },
               ),
-              onPressed: _fetchRandomJoke,
-              child: const Text('Fetch Random Joke'),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                final savedJokes = await _getSavedJokes();
-                showDialog(
-                  context: context,
-                  builder: (context) => AlertDialog(
-                    title: const Text('Saved Jokes'),
-                    content: SingleChildScrollView(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children:
-                            savedJokes.map((joke) => Text('* $joke')).toList(),
+            const Spacer(), // To push the buttons to the bottom
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.lightBlueAccent,
+                        padding: EdgeInsets.zero, // Padding set to zero
                       ),
+                      onPressed: _isLoading ? null : _fetchRandomJoke,
+                      child: const Text('Fetch Joke'),
                     ),
-                    actions: [
-                      TextButton(
-                        onPressed: () => Navigator.pop(context),
-                        child: const Text('Close'),
-                      ),
-                    ],
                   ),
-                );
-              },
-              child: const Text('View Saved Jokes'),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.lightGreen,
+                        padding: EdgeInsets.zero, // Padding set to zero
+                      ),
+                      onPressed: _isLoading
+                          ? null
+                          : () async {
+                              final joke = await _jokeFuture;
+                              _saveJoke(joke);
+                            },
+                      child: const Text('Save Joke'),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.orangeAccent,
+                        padding: EdgeInsets.zero, // Padding set to zero
+                      ),
+                      onPressed: () async {
+                        final savedJokes = await _getSavedJokes();
+                        showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            title: const Text('Saved Jokes'),
+                            content: SingleChildScrollView(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: savedJokes
+                                    .map((joke) => Text('* $joke'))
+                                    .toList(),
+                              ),
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () => Navigator.pop(context),
+                                child: const Text('Close'),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                      child: const Text('View Saved Jokes'),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                final joke = await _jokeFuture;
-                _saveJoke(joke);
-              },
-              child: const Text('Save Joke'),
-            ),
-            const SizedBox(height: 20),
           ],
         ),
       ),
--- lib/screens/time_zone/time_zone_screen.dart
+++ lib/screens/time_zone/time_zone_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:multi_app/api/api_client.dart';
+import 'package:multi_app/helpers/dbHelper.dart';
 import 'package:multi_app/screens/time_zone/time_zone_widget.dart';
 
 class TimeZoneScreen extends StatefulWidget {
@@ -11,6 +12,7 @@ class TimeZoneScreen extends StatefulWidget {
 
 class _TimeZoneScreenState extends State<TimeZoneScreen> {
   final TimeZoneApiClient _apiClient = TimeZoneApiClient();
+  final DBHelper _dbHelper = DBHelper();
   String? _timeZone;
   String? _currentTime;
   String? _error;
@@ -33,6 +35,74 @@ class _TimeZoneScreenState extends State<TimeZoneScreen> {
         _error = e.toString();
         _isLoading = false;
       });
+    }
+  }
+
+  Future<void> _saveTimeZoneData() async {
+    if (_timeZone != null && _currentTime != null) {
+      Map<String, dynamic> row = {
+        'timeZone': _timeZone!,
+        'currentTime': _currentTime!,
+      };
+      await _dbHelper.insert('timezone', row);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: const Text('Data saved successfully!'),
+          behavior: SnackBarBehavior.floating,
+          margin: const EdgeInsets.only(top: 50.0, left: 10.0, right: 10.0),
+          duration: const Duration(seconds: 2),
+        ),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: const Text('No data to save!'),
+          behavior: SnackBarBehavior.floating,
+          margin: const EdgeInsets.only(top: 50.0, left: 10.0, right: 10.0),
+          duration: const Duration(seconds: 2),
+        ),
+      );
+    }
+  }
+
+  Future<void> _retrieveTimeZoneData() async {
+    List<Map<String, dynamic>> data = await _dbHelper.queryAll('timezone');
+    if (data.isNotEmpty) {
+      data = data.reversed
+          .toList(); // Reverse the order to show the latest data first
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Saved Time Zone Data'),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: data.map((entry) {
+                  return Text(
+                      'Time Zone: ${entry['timeZone']}\nCurrent Time: ${entry['currentTime']}\n');
+                }).toList(),
+              ),
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: const Text('No saved data found!'),
+          behavior: SnackBarBehavior.floating,
+          margin: const EdgeInsets.only(top: 50.0, left: 10.0, right: 10.0),
+          duration: const Duration(seconds: 2),
+        ),
+      );
     }
   }
 
@@ -61,17 +131,45 @@ class _TimeZoneScreenState extends State<TimeZoneScreen> {
                           )
                         : const SizedBox(), // Placeholder if no data fetched yet
           ),
-          const Spacer(), // To push the button to the bottom
+          const Spacer(), // To push the buttons to the bottom
 
           Padding(
             padding: const EdgeInsets.all(10),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.lightBlueAccent,
-                padding: EdgeInsets.zero, // Padding set to zero
-              ),
-              onPressed: _isLoading ? null : _fetchTimeZoneData,
-              child: const Text('Fetch Time Zone Data'),
+            child: Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.lightBlueAccent,
+                      padding: EdgeInsets.zero, // Padding set to zero
+                    ),
+                    onPressed: _isLoading ? null : _fetchTimeZoneData,
+                    child: const Text('Fetch Data'),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.lightGreen,
+                      padding: EdgeInsets.zero, // Padding set to zero
+                    ),
+                    onPressed: _isLoading ? null : _saveTimeZoneData,
+                    child: const Text('Save Data'),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orangeAccent,
+                      padding: EdgeInsets.zero, // Padding set to zero
+                    ),
+                    onPressed: _retrieveTimeZoneData,
+                    child: const Text('Retrieve Data'),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
